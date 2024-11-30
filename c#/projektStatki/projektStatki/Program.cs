9aabc623d99ba9013083d6bced8c124af1de931c
Antoni Eryk Osial <antonierykosial@gmail.com>
2024-11-30 18:21:19 +0100

    Zmiana struktury katalogów

---
+++ c#/projektStatki/projektStatki/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projektStatki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("chuj ci w dupe!");
+        }
+    }
+}
